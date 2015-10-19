7b0a7bed2a13fe47c96a5c03b88c7a1f00f604b1
valdecar <valdecar@ya.ru>
2015-10-19 12:09:23 +0300

    add new table messages1, removed table's reciver and messages change ajax function's for that reason

--- messenger/ajax.py
+++ messenger/ajax.py
@@ -1,6 +1,10 @@
 from django.utils import simplejson
 from dajaxice.decorators import dajaxice_register
-from messenger.models import users,recivers,messages
+from dajax.core import Dajax
+from messenger.models import *
+
+
+
 
 @dajaxice_register
 def updateHistory(request,activeUserName):
@@ -8,7 +12,8 @@ def updateHistory(request,activeUserName):
     #get user object with name activeUserName
     activeUser=users.objects.get(name=activeUserName)
     #find messages for this user
-    m=messages.objects.filter(reciver=recivers.objects.get(user=activeUser))
+    #m=messages.objects.filter(reciver=recivers.objects.get(user=activeUser))
+    m =messages1.objects.filter(reciver=activeUser)
     l=zip([mm.mes for mm in m],[mm.sender.name for mm in m])
     #send it back
     return simplejson.dumps({'m':l})
@@ -16,26 +21,38 @@ def updateHistory(request,activeUserName):
 
 @dajaxice_register
 def sendMessage(request,activeUserName,reciver,message):
-    '''create message in dataBase'''
+    '''create message in dataBase
+    activeUserName,reciver is a stting's'''
+
     status = 'done'
     activeUser=users.objects.get(name=activeUserName)
     if len(message)!=0 and len(reciver)!=0:
         try:
             #trying save a message
-            reciverUser=users.objects.get(name=reciver)
-            r = recivers.objects.get(user=reciverUser)
+            reciverObject=users.objects.get(name=reciver)
+            #r = recivers.objects.get(user=reciverUser)
             #create an record 
-            m = messages(sender=activeUser,reciver=r,mes=message)
+            m = messages1(sender=activeUser,reciver=reciverObject,mes=message)
             m.save()
         except users.DoesNotExist:
             print('reciver not exist')
             status='error: reciver not exist'
     return simplejson.dumps({'status':status})
 
+
+
 @dajaxice_register
 def mult(request,a,b):
     res=int(a)*int(b)
     return simplejson.dumps({'res':str(res)})
+
+@dajaxice_register
+def multiply(request, a, b):
+    dajax = Dajax()
+    result = int(a) * int(b)
+    dajax.assign('#result','value',str(result))
+    return dajax.json()
+
 @dajaxice_register
 def sayhello(request):
     return simplejson.dumps({'message':'Hello World'})
--- messenger/models.py
+++ messenger/models.py
@@ -5,7 +5,7 @@ class users(models.Model):
     name = models.CharField(max_length=30)
     password = models.CharField(max_length=30)
     status = models.BooleanField(default=False)
-
+'''
 class recivers(models.Model):
     user =models.OneToOneField(users)
     #models.ForeignKey(users)
@@ -16,5 +16,10 @@ class messages(models.Model):
     reciver=models.ForeignKey(recivers) 
     mes=models.CharField(max_length=100)    
     time=models.DateTimeField(auto_now=True)
-
+'''
+class messages1(models.Model):
+    sender=models.ForeignKey('users',related_name='users1')
+    reciver=models.ForeignKey('users',related_name='users2') 
+    mes=models.CharField(max_length=100)    
+    time=models.DateTimeField(auto_now=True)
 
